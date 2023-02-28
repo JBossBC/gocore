ef21a6071c8715565184361dcc0dbde704983788
xiyang <90125263+JBossBC@users.noreply.github.com>
2023-02-28 20:34:50 +0800

    search query coding

---
+++ ethereum_util/concurrent_searchTest.go
@@ -0,0 +1,22 @@
+package ethereum_util
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"testing"
+)
+
+func TestGetLogs(t *testing.T) {
+	//number, _ := GetCurrentBlockNumber()
+	event, err := GetEvent(TimeLess, 39000000, 39791976, []common.Address{common.HexToAddress("0xC7728354f9fe0e43514B1227162D5B0E40FaD410")}, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	done, err := event.Done()
+
+	if err != nil {
+		return
+	}
+	fmt.Println(len(done))
+}
